7507c6e272a9e661de4f9fe699b642a9e28cf93e
Mohammed Usama <usamabhakrani@gmail.com>
2023-08-01 17:35:36 +0500

    Declutter

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,13 @@ import React from "react";
 import Test from "./components/Test";
 import styled from "styled-components";
 
-
 const App = () => {
   return (
     <>
-      <Test color='dodgerblue' />
-      <Test color='black' />
-      <Test color='coral' />
-      <Test color='pink' />
-      <Test color='dodgerblue' />
-      <Test color='dodgerblue' />
-      <Test color='dodgerblue' />
+      <Test color="dodgerblue" />
+      <Test color="black" />
+      <Test color="coral" />
+      <Test color="pink" />
     </>
   );
 };
